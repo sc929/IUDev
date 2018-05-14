59a48e3cdddc492cacb5582bcce2de315cdcf1a4
Maxim <sc929@yandex.ru>
2018-05-14 20:54:23 +0300

    Добавлено все, что есть

---
+++ IUGameDev/main.cpp
@@ -0,0 +1,41 @@
+#include "GameStart.h"
+
+GameStart *game = nullptr;
+
+int main(int argc, char *argv[])
+{
+	const int FPS = 60;
+	const int frameDelay = 1000 / FPS;
+
+	Uint32 frameStart;
+	int frameTime;
+
+	game = new GameStart();
+
+	game->init("SUPERgame", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 800, 600, false);
+
+	while (game->running())
+	{
+		frameStart = SDL_GetTicks();
+
+
+		game->handleEvents();
+		game->update();
+		game->render();
+
+		frameTime = SDL_GetTicks() - frameStart;
+
+		// При необходимости расчитать передвижение при низком FPS 
+		//(для мультиплеера например)
+		// Или сделать отдельную функцию fixedUpdate()
+		if (frameDelay > frameTime)
+		{
+			SDL_Delay(frameDelay - frameTime);
+		}
+	}
+
+	game->clean();
+
+	return 0;
+}
+
